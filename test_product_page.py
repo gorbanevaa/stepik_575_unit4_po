e9dd8ef6f61b67b84f21d314931a53f28de7e321
Andrew <gorbanevaa@gmail.com>
2020-05-29 20:38:06 +0300

    mark link№7 xfail

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
 
   
-@pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+@pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
     
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
